8be7045753f5fb001c0cc33c768efc7ced15ea9a
JTPeng <404502753@qq.com>
2022-05-18 23:07:30 +0800

    directory sort

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,59 @@
 module.exports = {
-  base:"/vitepress/", // 基础路径
-  lang: 'em-US', // 语言
-  title: "Hello VitePress",
-  description: "Just playing around"
+  title: "Learn Notes",
+  lang: "zh-CN",
+  description: "Learn Notes",
+  head: [
+    // ["link", { rel: "icon", type: "image/svg+xml", href: "/logo.svg" }],
+    // ["script", { src: "", async: "" }],
+  ],
+  vue: {
+    reactivityTransform: true,
+  },
+  themeConfig: {
+    // logo: "/logo.svg",
+    docsBranch: "main",
+    editLinks: true,
+    nav: [
+      { text: "Vue", link: "/vueDoc/" },
+      { text: "React", link: "/reactDoc/" },
+      { text: "Javascript", link: "/JSDoc/" },
+      { text: "Css", link: "/cssDoc/" },
+      // {
+      //   text: "相关链接",
+      //   items: [
+      //     {
+      //       text: "",
+      //       link: "",
+      //     },
+      //   ],
+      // },
+    ],
+
+    sidebar: {
+      "/vueDoc":"auto",
+      "/reactDoc": "auto",
+      "/JSDoc": "auto",
+      "/cssDoc": "auto",
+      "/": [
+        {
+          text: "指引",
+          children: [
+            // {
+            //   text: "",
+            //   link: "",
+            // },
+          ],
+        },
+      ],
+    },
+  },
+
+  // markdown: {
+  //   anchor: {
+  //     renderPermalink: require("./render-perma-link"),
+  //   },
+  //   config: (md) => {
+  //     md.use(require("./markdown-it-custom-anchor"));
+  //   },
+  // },
 };
--- docs/index.md
+++ docs/index.md
@@ -2,14 +2,14 @@
 home: true
 # heroImage: /logo.png
 heroAlt: Logo image
-heroText: Hero Title
+heroText: Learn Notes
 tagline: Hero subtitle
 actionText: Get Started
 actionLink: /base/one
 features:
-  - title: Simplicity First
+  - title: Vue
     details: Minimal setup with markdown-centered project structure helps you focus on writing.
-  - title: Vue-Powered
+  - title: React
     details: Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop custom themes with Vue.
   - title: Performant
     details: VitePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
